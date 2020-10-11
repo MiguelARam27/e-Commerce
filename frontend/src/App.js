1d1afae3c500aa59580180f4eeea2b67d1b4d327
MiguelARam27 <garandster27@live.com>
2020-10-11 02:05:17 -0400

    Added HEader and footer components, and imp FA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React, { Fragment } from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>hello world</h1>
-    </div>
+    <Fragment>
+      <Header />
+      <Container>
+        <main className='py-3'>
+          <h1>welcome</h1>
+        </main>
+      </Container>
+      <Footer />
+    </Fragment>
   );
 }
 
