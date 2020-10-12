849cda3c25adf18be06dd274785fd9ab0c45e405
MiguelARam27 <garandster27@live.com>
2020-10-11 22:19:04 -0400

    Cards set up, images added, bootstrap template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <Container>
         <main className='py-3'>
-          <h1>welcome</h1>
+          <HomeScreen />
         </main>
       </Container>
       <Footer />
